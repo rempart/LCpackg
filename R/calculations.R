8701ffabf4fb03c0d39621e7839f3ccdcf0f0b65
Eric PARENT <eric.parent@agroparistech.fr>
2020-04-30 11:28:50 +0200

    premiers pas

---
+++ R/calculations.R
@@ -0,0 +1,25 @@
+#' Basics
+#'
+#'Some usefull operations
+#' @param x Numeric
+#' @param y Numeric
+#'
+#' @return Sum or product \code{x} and \code{y}
+#' @export
+#'
+#' @examples
+#' add(4,7)
+add<-function(x,y){
+  x+y
+}
+
+#' Title
+#'
+#' @inheritParams add
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' multiply(3,2.1)
+multiply <- function(x,y){ x*y}
